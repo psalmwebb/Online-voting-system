8d7394025cefadb064c45d1360e167e9654d0250
psalmwebb <psalmyweb@gmail.com>
2021-08-16 23:30:49 +0100

    few changes

--- conn.js
+++ conn.js
@@ -7,6 +7,8 @@ const dbName = process.env.MONGODB_NAME
 
 const PORT = process.env.PORT || 5000
 
+const localDbURL = "mongodb://localhost/voting-system"
+
 const remoteDbURL = `mongodb+srv://${dbUser}:${dbPass}@cluster0.gqtmu.mongodb.net/${dbName}?retryWrites=true&w=majority`
 
 
