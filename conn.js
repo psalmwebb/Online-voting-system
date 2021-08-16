0c8ca9208a7379a8235b05602ccff20e07537351
psalmwebb <psalmyweb@gmail.com>
2021-08-16 22:24:52 +0100

    changed port

--- conn.js
+++ conn.js
@@ -5,6 +5,7 @@ const dbPass = process.env.MONGODB_PASSWORD
 const dbUser = process.env.MONGODB_USERNAME
 const dbName = process.env.MONGODB_NAME
 
+const PORT = process.env.PORT || 5000
 
 const remoteDbURL = `mongodb+srv://${dbUser}:${dbPass}@cluster0.gqtmu.mongodb.net/${dbName}?retryWrites=true&w=majority`
 
@@ -14,7 +15,7 @@ module.exports = (app)=>{
     mongoose.connect(remoteDbURL,{useNewUrlParser:true,useUnifiedTopology:true})
 
     .then(()=>{
-        app.listen(5000,()=> console.log("Serving at port : 5000"))
+        app.listen(PORT,()=> console.log("Serving at port : 5000"))
     })
     .catch(err=>{
        
